5a665ed3df8d39a7244902c9e7278478bb8baa1d
ManuelSchmidt707 <131887717+ManuelSchmidt707@users.noreply.github.com>
2024-07-15 14:59:13 +0200

    Add ResNeXt to CNN models

--- cnn_models.py
+++ cnn_models.py
@@ -193,3 +193,111 @@ class CNN_CNL(nn.Module):
     def forward(self, x):
         x = self.layer1(x.unsqueeze(1)).squeeze()
 
+class ResNeXt_Block(torch.nn.Module):
+    '''
+    The Convolution Block used by the ResNeXt model.
+    '''
+    def __init__(self, inchannels, outchannels, stride = 1, downsample = None, bias = False, dropout = 0, cardinality = 16):
+
+        super(ResNeXt_Block, self).__init__()
+        
+        self.bn1 = torch.nn.BatchNorm2d(inchannels)
+        self.bn2 = torch.nn.BatchNorm2d(inchannels)
+        self.bn3 = torch.nn.BatchNorm2d(outchannels)    
+        self.relu = torch.nn.ReLU(inplace=True)
+
+        self.conv1x1_down = torch.nn.Conv2d(inchannels, inchannels, kernel_size=1, stride=1, bias=bias)
+        self.conv3x3 = torch.nn.Conv2d(inchannels, inchannels, kernel_size=3, stride=stride, padding=1, groups=cardinality, bias=bias)
+        self.conv1x1_up = torch.nn.Conv2d(inchannels, outchannels, kernel_size=1, stride=1, bias=bias)
+
+        self.downsample = downsample
+        self.stride = stride
+        self.cardinality = cardinality
+
+    def forward(self, x):
+        id = x 
+
+        out = self.conv1x1_down(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv3x3(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv1x1_up(out)
+        out = self.bn3(out)
+        
+        if self.downsample is not None:
+            id = self.downsample(x)
+
+        out += id
+        out = self.relu(out)
+
+        return out
+
+class ResNeXt(nn.Module):
+    '''
+    The Implementation of the ResNeXt model. 
+    '''
+    def __init__(self, Block_type, layers, num_classes = 2, inchannels = 3, bias = False, k = 1, dropout = 0, cardinality = 16):
+        super(ResNeXt, self).__init__()
+    
+        if k * 64 < cardinality:
+            warnings.warn("The number of groups in the ResNeXt model (cardinality) is bigger than the number of channels. This is not allowed. The number of groups will be reduced.")
+            cardinality = int(k * 64/2)
+            warnings.warn("The number of groups (cardinality) is now set to: " + str(cardinality))
+
+        self.k = k
+        self.hight = 64 * self.k
+        self.cardinality = cardinality
+
+        self.input_layer = torch.nn.Sequential(
+            torch.nn.Conv2d(inchannels, self.hight, kernel_size=7,stride=2,padding=10,bias=bias),
+            torch.nn.BatchNorm2d(self.hight),
+            torch.nn.ReLU(inplace=True)
+        )
+
+        self.layer1 = self.make_layer(Block_type, self.hight, layers[0], bias = bias, dropout=dropout, cardinality = self.cardinality)
+        self.layer2 = self.make_layer(Block_type, self.hight * 2, layers[1], stride = 2, bias = bias, dropout=dropout, cardinality = self.cardinality)
+        self.layer3 = self.make_layer(Block_type, self.hight * 2, layers[2], stride = 2, bias = bias, dropout=dropout, cardinality = self.cardinality)
+
+        self.avgpool = torch.nn.AdaptiveAvgPool2d((1,1))
+        self.classifier = torch.nn.Linear(self.hight, num_classes)
+
+    def make_layer(self, Block_type, channels, num_blocks, stride = 1, bias = None, dropout = 0, cardinality = 32):
+
+        downsample = None
+
+        if stride != 1 or self.hight != channels:
+            downsample = torch.nn.Sequential( 
+                torch.nn.Conv2d(self.hight, channels, kernel_size=1, stride=stride, bias=bias),
+                torch.nn.BatchNorm2d(channels)
+            )
+
+        layer = []
+
+        for i in range(1, num_blocks):
+            layer.append(Block_type(self.hight, self.hight, bias = bias, dropout = dropout, cardinality = cardinality))
+
+        layer.append(Block_type(self.hight, channels, stride, downsample, bias = bias, dropout = dropout, cardinality = cardinality))
+
+        self.hight = channels
+
+        return torch.nn.Sequential(*layer)
+
+    def forward(self, x):
+        if self.input == '3D':
+            x = self.input_layer(x.unsqueeze(1)).squeeze()
+        else:
+            x = self.input_layer(x)
+
+        x = self.layer1(x)          
+        x = self.layer2(x)          
+        x = self.layer3(x)                  
+
+        x = self.avgpool(x)         
+        x = torch.flatten(x, 1)     
+        x = self.classifier(x)
+
+        return x
